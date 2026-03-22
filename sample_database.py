3ae60ad07a7e95a23219beb59ebdec5b85c1317e
prateEKsaha07 <155552003+prateEKsaha07@users.noreply.github.com>
2026-03-22 22:31:00 +0530

    added a bit more data to work with with a different table to try to use joins in them

--- sample_database.py
+++ sample_database.py
@@ -1,4 +1,5 @@
 import sqlite3
+import random
 
 conn = sqlite3.connect("database.db")
 cursor = conn.cursor()
@@ -12,8 +13,37 @@ CREATE TABLE IF NOT EXISTS students (
 )
 """)
 
-cursor.execute("INSERT INTO students (name, age, marks) VALUES ('Prateek', 21, 85)")
-cursor.execute("INSERT INTO students (name, age, marks) VALUES ('Rahul', 22, 90)")
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS courses (
+    course_id INTEGER PRIMARY KEY,
+    course_name TEXT,
+    student_id INTEGER,
+    FOREIGN KEY(student_id) REFERENCES students(id)
+)
+""")
+
+
+names = ["Amit", "Sneha", "Rohit", "Anjali", "Karan", "Priya", "Vikas", "Neha"]
+conn = sqlite3.connect("database.db")
+cursor = conn.cursor()
+
+for i in range(50):
+    name = random.choice(names)
+    age = random.randint(18, 25)
+    marks = random.randint(50, 100)
+
+    cursor.execute(
+        "INSERT INTO students (name, age, marks) VALUES (?, ?, ?)",
+        (name, age, marks)
+    )
+
+courses = ["Math", "Science", "CS", "English"]
+
+for i in range(50):
+    cursor.execute(
+        "INSERT INTO courses (course_name, student_id) VALUES (?, ?)",
+        (random.choice(courses), random.randint(1, 10))
+    )
 
 conn.commit()
 conn.close()
